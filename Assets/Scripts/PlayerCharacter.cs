b12a2780ff96377d79c13824f20d57ff1566611f
William Terry <Tezza48is@aol.com>
2017-11-22 18:33:16 +0000

    Physics and camera effect working with debug movement

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+
+    public int layerA = 8, layerB = 9;
+
+    bool isLayerA = true;
+
+    public GameObject collisionObject;
+
+    public IE_DualCameraBlend cameraEffect;
+
+    public Rigidbody2D m_Rigid;
+    public Animator m_Anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        collisionObject.layer = isLayerA ? layerA : layerB;
+
+        m_Anim.SetBool("isZoneA", isLayerA);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            isLayerA = !isLayerA;
+            collisionObject.layer = isLayerA ? layerA : layerB;
+
+            m_Anim.SetBool("isZoneA", isLayerA);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_Rigid.AddForce(Vector2.up * 20, ForceMode2D.Impulse);
+        }
+
+        m_Rigid.velocity = m_Rigid.velocity.y * Vector2.up + 20 * Vector2.right;
+
+    }
+}
