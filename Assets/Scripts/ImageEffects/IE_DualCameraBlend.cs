6855246b8e883029a8b36cd8600746255c0a4317
William Terry <Tezza48is@aol.com>
2017-11-22 17:28:14 +0000

    Shader effect implemented

---
+++ Assets/Scripts/ImageEffects/IE_DualCameraBlend.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class IE_DualCameraBlend : MonoBehaviour
+{
+
+    public Shader shader;
+    public Texture2D Transition;
+    [Range(0.0f, 1.01f)]
+    public float cutoff = 0.5f;
+
+    private Camera mainCamera;
+    public Camera secondaryCamera;
+
+    RenderTexture rTarget;
+
+    private Material effectMaterial;
+
+    // Use this for initialization
+    void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+        //gameObject.layer = LayerMask.GetMask("CaptureA");
+
+        rTarget = new RenderTexture(Screen.width, Screen.height, 24);
+        secondaryCamera.targetTexture = rTarget;
+
+        effectMaterial = new Material(shader);
+
+        //Shader.SetGlobalTexture("_SecondTex", targetA);
+        //Shader.SetGlobalTexture("_Transition", Transition);
+
+        effectMaterial.SetTexture("_SecondTex", rTarget);
+        effectMaterial.SetTexture("_Transition", Transition);
+
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        effectMaterial.SetFloat("_Cutoff", cutoff);
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
