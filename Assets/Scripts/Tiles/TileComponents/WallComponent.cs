454accf85c68fa3f7065866c1027afbcc2af616c
William Terry <Tezza48is@aol.com>
2017-11-29 13:28:03 +0000

    Wall tile created, next is impassable wall tile

---
+++ Assets/Scripts/Tiles/TileComponents/WallComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallComponent : MonoBehaviour
+{
+
+    public Transform wall;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Resize(float newPosition)
+    {
+        wall.localPosition = new Vector2(newPosition, wall.localPosition.y);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
